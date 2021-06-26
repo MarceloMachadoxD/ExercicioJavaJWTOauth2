e866e1daf4ccd01ee0ac316dd4d21a7fa9abb2ff
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-26 15:39:45 -0300

    Adjust user roles permissions

--- src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
@@ -25,9 +25,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
-    private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
+    private static final String[] OPERATOR_GET = {"/departments/**", "/employees/**"};
 
-    private static final String[] ADMIN = {"/users/**"};
+
 
 
 
@@ -47,10 +47,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
         http.authorizeRequests()
             .antMatchers(PUBLIC).permitAll()
-            .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-            .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
-            .antMatchers(ADMIN).hasRole("ADMIN")
-            .anyRequest().authenticated();
+            .antMatchers(HttpMethod.GET, OPERATOR_GET).hasAnyRole("OPERATOR", "ADMIN")
+            .anyRequest().hasAnyRole("ADMIN");
 
     }
 }
